c575fe0393b072b0e474a706872dcf12364109e9
MAX F.C <maxgt734@gmail.com>
2020-07-25 18:22:13 -0600

    Se definió un icono por defecto a las notificaciones

--- Android/app/src/main/java/com/max/wapp_sticker_store/Firebase/MyFirebaseMessagingService.java
+++ Android/app/src/main/java/com/max/wapp_sticker_store/Firebase/MyFirebaseMessagingService.java
@@ -1,11 +1,13 @@
 package com.max.wapp_sticker_store.Firebase;
 
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Build;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -32,23 +34,33 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     public void mostrarNotificacion(String titulo, String cuerpo){
-
+        final String CHANNEL_1_ID = "channel1";
         Intent newIntent = new Intent(this, MainActivity.class);
         newIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, newIntent, PendingIntent.FLAG_ONE_SHOT);
         Uri uriSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
-                .setSmallIcon(R.drawable.icono)
-                .setContentTitle(titulo)
-                .setContentText(cuerpo)
-                .setAutoCancel(true)
-                .setSound(uriSound)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .setContentIntent(pendingIntent);
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            NotificationChannel channel1 = new NotificationChannel(
+                    CHANNEL_1_ID,
+                    "Channel 1",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel1.setDescription("Canal de Notificaciones 1");
+            NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this,CHANNEL_1_ID)
+                    .setSmallIcon(R.drawable.icono)
+                    .setContentTitle(titulo)
+                    .setContentText(cuerpo)
+                    .setAutoCancel(true)
+                    .setSound(uriSound)
+                    .setPriority(NotificationCompat.PRIORITY_HIGH)
+                    .setCategory(NotificationCompat.CATEGORY_MESSAGE)
+                    .setContentIntent(pendingIntent);
 
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(0,notificationBuilder.build());
+            NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(channel1);
+            notificationManager.notify(0,notificationBuilder.build());
+        }
     }
 }
 
